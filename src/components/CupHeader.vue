ef3cec9027bf94494bd9e3160e872bc2726d0b4c
jaonoctus <joaodias@noctus.org>
2018-01-30 17:14:47 -0200

    Increase cup noodles logo size

--- src/components/CupHeader.vue
+++ src/components/CupHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col-md-12 text-center">
-      <img src="~@/assets/logo-cn.png" class="logo-cn my-5"/>
+      <img src="~@/assets/logo-cn.png" class="logo-cn mt-5 mb-3"/>
       <h2>#DescubraCup<br/><small>#CPBR11</small></h2>
     </div>
   </div>
@@ -14,6 +14,6 @@ export default {
 
 <style scoped>
 .logo-cn {
-  width: 150px;
+  width: 200px;
 }
 </style>
